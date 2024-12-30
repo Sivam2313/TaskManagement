1df9cd321d4e02f5456c1fb361e30d061db533e4
sivam <sivamdas7@gmail.com>
2024-12-30 10:57:47 +0530

    added production url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = process.env.REACT_APP_API || "http://localhost:5000";
+const api = process.env.REACT_APP_API || "https://taskmanagement-hehd.onrender.com";
 
 export default axios.create({
   baseURL: api,
